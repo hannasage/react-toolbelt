ea17748159d033e63765318cc18ed4fce96cc058
Kevin Haube <booking@kevinmhaube.com>
2022-03-16 20:06:31 -0400

    Typing and refactor

--- src/network/hooks/UseCursorManager.ts
+++ src/network/hooks/UseCursorManager.ts
@@ -1,13 +1,30 @@
-import { useCallback, useState } from "react";
+import {useCallback, useMemo, useState} from "react";
 
+type CursorMap = Map<number, string>
+
+interface ICursorValues {
+    cursors: CursorMap,
+    currentIndex: number,
+    hasPrev: boolean,
+    hasNext: boolean
+}
+
+interface ICursorController {
+    addNextCursor: (val: string) => void
+}
+
+/* CursorManager handles logic to maintain an accurate map of cursors by page.
+* Each time you append a cursor with `addNextCursor`, the manager will handle,
+* duplicates and set your hasNext and hasPrev values for you. */
 const useCursorManager = () => {
-    const [cursors, setCursors] = useState<Map<number, string>>(
+    const [cursors, setCursors] = useState<CursorMap>(
         new Map<number, string>([[0, ""]])
     );
     const [currentIndex, setCurrentIndex] = useState<number>(0);
     const [hasPrev, setHasPrev] = useState<boolean>(false);
     const [hasNext, setHasNext] = useState<boolean>(false);
 
+    /* Handles verifying cursor duplication */
     const cursorExists = useCallback(
         (c: string) => {
             return Array.from(cursors.values()).includes(c);
@@ -15,6 +32,8 @@ const useCursorManager = () => {
         [cursors]
     );
 
+    /* Private method for handling cursor addition since we may not always
+    * want to just append a cursor. */
     const addCursor = (index: number, val: string) => {
         if (!cursorExists(val)) {
             setCursors(cursors.set(index, val))
@@ -23,11 +42,26 @@ const useCursorManager = () => {
         setHasPrev(cursors.get(currentIndex - 1) !== undefined)
     }
 
+    /* Handles simple appending of new cursors to the end of the Map */
     const addNextCursor = (val: string) => {
         addCursor(cursors.size, val)
     }
 
-    return { cursors, currentIndex, hasPrev, hasNext, addNextCursor, setCurrentIndex };
+    /* Returning values and controllers as objects to keep them clean */
+    const values: ICursorValues = useMemo(() => {
+        return {
+            cursors: cursors,
+            currentIndex: currentIndex,
+            hasPrev: hasPrev,
+            hasNext: hasNext
+        }
+    }, [cursors, currentIndex, hasPrev, hasNext])
+
+    const controller: ICursorController = {
+        addNextCursor: addNextCursor,
+    }
+
+    return { values, controller };
 };
 
 export default useCursorManager;
--- src/network/hooks/UseCursorMap.test.ts
+++ src/network/hooks/UseCursorMap.test.ts
@@ -5,22 +5,22 @@ describe("useCursorMap", () => {
     test("Hook renders with default values", () => {
         const { result } = renderHook(() => useCursorManager());
 
-        expect(result.current.cursors).toEqual(
+        expect(result.current.values.cursors).toEqual(
             new Map<number, string>([[0, ""]])
         );
-        expect(result.current.currentIndex).toBe(0);
-        expect(result.current.hasPrev).toBe(false);
-        expect(result.current.hasNext).toBe(false);
+        expect(result.current.values.currentIndex).toBe(0);
+        expect(result.current.values.hasPrev).toBe(false);
+        expect(result.current.values.hasNext).toBe(false);
     });
 
     test("addNextCursor successfully appends map with cursor", () => {
         const { result } = renderHook(() => useCursorManager());
 
         act(() => {
-            result.current.addNextCursor("test");
+            result.current.controller.addNextCursor("test");
         });
 
-        expect(result.current.cursors).toEqual(new Map([
+        expect(result.current.values.cursors).toEqual(new Map([
             [0, ""],
             [1, "test"]
         ]));
@@ -34,35 +34,35 @@ describe("useCursorMap", () => {
         /* After first response, add cursors */
         act(() => {
             response.forEach((c: string) => {
-                result.current.addNextCursor(c)
+                result.current.controller.addNextCursor(c)
             })
         })
 
-        expect(result.current.cursors).toEqual(new Map<number, string>([
+        expect(result.current.values.cursors).toEqual(new Map<number, string>([
             [0, ""],
             [1, "123"],
             [2, "456"],
             [3, "789"]
         ]))
-        expect(result.current.hasNext).toBe(true)
-        expect(result.current.hasPrev).toBe(false)
+        expect(result.current.values.hasNext).toBe(true)
+        expect(result.current.values.hasPrev).toBe(false)
 
         /* Paging to the next page would mean you'd get two duplicate cursors and
          * one NEW cursor in this array; this should only add the new cursor. */
         act(() => {
             responseTwo.forEach((c: string) => {
-                result.current.addNextCursor(c)
+                result.current.controller.addNextCursor(c)
             })
         })
 
-        expect(result.current.cursors).toEqual(new Map<number, string>([
+        expect(result.current.values.cursors).toEqual(new Map<number, string>([
             [0, ""],
             [1, "123"],
             [2, "456"],
             [3, "789"],
             [4, "000"]
         ]))
-        expect(result.current.hasNext).toBe(true)
-        expect(result.current.hasPrev).toBe(false)
+        expect(result.current.values.hasNext).toBe(true)
+        expect(result.current.values.hasPrev).toBe(false)
     })
 });
